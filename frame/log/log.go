027282e302eae3eb3b8cecffc077948720f20636
yuyansong <1259581033@qq.com>
2025-02-27 22:57:34 +0800

    日志记录相关内容——分级日志

--- frame/log/log.go
+++ frame/log/log.go
@@ -49,6 +49,7 @@ func (l LoggerLevel) Level() string {
 
 // 日志级别常量
 const (
+	// 使用 LoggerLevel 定义日志级别
 	LevelDebug LoggerLevel = iota
 	LevelInfo
 	LevelError
@@ -113,42 +114,54 @@ func Default() *Logger {
 
 // Info 记录一条 INFO 级别的日志
 func (l *Logger) Info(msg any) {
+	// 调用 Print 方法记录一条 INFO 级别的日志
 	l.Print(LevelInfo, msg)
 }
 
 // Debug 记录一条 DEBUG 级别的日志
 func (l *Logger) Debug(msg any) {
+	// 调用 Print 方法记录一条 DEBUG 级别的日志
 	l.Print(LevelDebug, msg)
 }
 
 // Error 记录一条 ERROR 级别的日志
 func (l *Logger) Error(msg any) {
+	// 调用 Print 方法记录一条 ERROR 级别的日志
 	l.Print(LevelError, msg)
 }
 
 // Print 根据指定的日志级别和消息打印日志
 func (l *Logger) Print(level LoggerLevel, msg any) {
+	// 检查当前日志级别是否高于输入级别
 	if l.Level > level {
 		// 如果当前日志级别高于输入级别，则不打印日志
 		return
 	}
+	// 创建 LoggingFormatParam
 	param := &LoggingFormatParam{
 		Level:        level,
 		LoggerFields: l.LoggerFields,
 		Msg:          msg,
 	}
+	// 使用 Formatter 格式化日志信息
 	str := l.Formatter.Format(param)
+	// 遍历所有输出目的地
 	for _, out := range l.Outs {
+		// 如果输出目的地是标准输出，则启用颜色模式
 		if out.Out == os.Stdout {
 			param.IsColor = true
+			// 重新格式化日志信息以应用颜色模式
 			str = l.Formatter.Format(param)
 			fmt.Fprintln(out.Out, str)
 		}
+		// 如果输出级别的设置为 -1 或与当前日志级别相同，则打印日志
 		if out.Level == -1 || level == out.Level {
 			fmt.Fprintln(out.Out, str)
+			// 检查并可能处理日志文件的大小
 			l.CheckFileSize(out)
 		}
 	}
+
 }
 
 // WithFields 返回一个新的 Logger 实例，并添加额外的日志字段
@@ -163,45 +176,67 @@ func (l *Logger) WithFields(fields Fields) *Logger {
 
 // SetLogPath 设置日志文件路径，并初始化多个日志文件输出
 func (l *Logger) SetLogPath(logPath string) {
+	// 设置日志路径并初始化不同级别的日志输出
+	// logPath 是日志文件的目录路径
 	l.logPath = logPath
+
+	// 添加记录所有级别日志的输出
 	l.Outs = append(l.Outs, &LoggerWriter{
-		Level: -1,
+		Level: -1, // -1 表示记录所有级别的日志
 		Out:   FileWriter(path.Join(logPath, "all.log")),
 	})
+
+	// 添加记录调试级别日志的输出
 	l.Outs = append(l.Outs, &LoggerWriter{
-		Level: LevelDebug,
+		Level: LevelDebug, // 仅记录调试级别的日志
 		Out:   FileWriter(path.Join(logPath, "debug.log")),
 	})
+
+	// 添加记录信息级别日志的输出
 	l.Outs = append(l.Outs, &LoggerWriter{
-		Level: LevelInfo,
+		Level: LevelInfo, // 仅记录信息级别的日志
 		Out:   FileWriter(path.Join(logPath, "info.log")),
 	})
+
+	// 添加记录错误级别日志的输出
 	l.Outs = append(l.Outs, &LoggerWriter{
-		Level: LevelError,
+		Level: LevelError, // 仅记录错误级别的日志
 		Out:   FileWriter(path.Join(logPath, "error.log")),
 	})
 }
 
 // CheckFileSize 检查日志文件大小，如果超过限制则创建新的日志文件
 func (l *Logger) CheckFileSize(w *LoggerWriter) {
+	// 获取日志文件对象
 	logFile := w.Out.(*os.File)
+	// 检查日志文件是否有效
 	if logFile != nil {
+		// 获取日志文件的信息
 		stat, err := logFile.Stat()
 		if err != nil {
+			// 如果获取文件信息失败，则记录错误并退出
 			log.Println(err)
 			return
 		}
+		// 获取日志文件的大小
 		size := stat.Size()
+		// 设置默认的最大日志文件大小为 100MB，如果未设置的话
 		if l.LogFileSize <= 0 {
 			l.LogFileSize = 100 << 20 // 默认最大日志文件大小为 100MB
 		}
+		// 检查当前日志文件大小是否超过了最大限制
 		if size >= l.LogFileSize {
+			// 获取当前日志文件的名称
 			_, name := path.Split(stat.Name())
+			// 提取文件名，去除扩展名
 			fileName := name[0:strings.Index(name, ".")]
+			// 创建一个新的日志文件写入对象，文件名基于原始文件名和当前时间戳
 			writer := FileWriter(path.Join(l.logPath, string_func.JoinStrings(fileName, ".", time.Now().UnixMilli(), ".log")))
+			// 更新输出流为新的日志文件
 			w.Out = writer
 		}
 	}
+
 }
 
 // format 格式化日志消息
